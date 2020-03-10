275896f2e93a4def8bb3f6c0df675c30a2e7ae88
W1ckedD <alirezachoobineh@ymail.com>
2020-03-10 20:01:59 +0330

    Added mongodb

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(
+            'mongodb://localhost:27017/tracker',
+            {
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useUnifiedTopology: true
+            }
+        );
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 
 // App
 const app = express();
 
+// Database
+connectDB();
+
+// Middlewares
 app.get('/', (req, res, next) => {
     res.send('Hello World');
 });
 
-
 // Port
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
